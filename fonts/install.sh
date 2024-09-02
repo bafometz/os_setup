c7a6d6a1d02f9b2a038030518ea1ceeccd5128c8
Bafometz <pro100swt@gmail.com>
2024-09-02 18:20:45 +0300

    updated fonts installation

--- fonts/install.sh
+++ fonts/install.sh
@@ -1,13 +1,57 @@
-#!/bin/sh
+#!/bin/bash
 
-cd go 
-sh install.sh
-cd ../
+set -eu
 
-cd fonts
-sh install.sh
-cd ../
+extract() {
+    case $(file -b "$1") in
+      *tar.bz2) tar xjf "$1" ;;
+      *tar.gz|*.tgz) tar xzf "$1" ;;
+      *tar.xz) tar xJf "$1" ;;
+      *zip) unzip "$1" ;;
+      *7z) 7z x "$1" ;;
+      *rar) unrar x "$1" ;;
+      *gzip|*.gz) gunzip "$1" ;;
+      *bzip2|*.bz2) bunzip2 "$1" ;;
+      *xz) unxz "$1" ;;
+      *) echo "Unknown archive type: $1" ;;
+    esac
+}
 
-cd FontPatcher
-python font-patcher
-cd ../
+
+fonts_url=("https://www.fontsquirrel.com/fonts/download/go"
+ "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Go-Mono.zip"
+ "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Lilex.zip")
+
+download_file="go.zip"
+unzip_folder="go_fonts"
+
+cd "/tmp"
+rm -rf "${download_file}" 
+rm -rf "${unzip_folder}"
+
+for url in "${fonts_url[@]}"
+do
+  rm -rf "${download_file}" 
+  rm -rf "${unzip_folder}"
+
+  echo "Start download: ${url}"
+  curl -L --output "${download_file}" "${url}" 1> /dev/null 2> /dev/null
+
+  echo "Download done: ${download_file}"
+
+  echo "Unzip: ${download_file} to ${unzip_folder}"
+  unzip "${download_file}" -d "${unzip_folder}" 1> /dev/null 2> /dev/null
+
+  cd "/tmp/${unzip_folder}"
+  echo "Install go fonts"
+  find . -type f -name "*.ttf" -exec cp -f {} "/home/${USER}/.local/share/fonts" \; 1> /dev/null 2> /dev/null
+  echo "All installed"
+  cd "../"
+
+  echo  "Cleanup"
+  rm -rf "${download_file}" 
+  rm -rf "${unzip_folder}"
+  echo "
+  
+  "
+done
